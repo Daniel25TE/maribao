6bc6be28e2f877fb870f444951e4f9f429a79f75
Daniel25TE <llu24001@byui.edu>
2025-04-18 07:24:39 -0600

    maribao 5.0

--- scripts/maribao.js
+++ scripts/maribao.js
@@ -1,6 +1,9 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
 
+const currentYearMobile = new Date().getFullYear();
+document.getElementById('currentyear-mobile').textContent = currentYearMobile;
+
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
 
