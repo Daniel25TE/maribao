abd3feb483a21879c6437152b969b992b9c8c3a0
Daniel25TE <llu24001@byui.edu>
2025-04-17 21:43:58 -0600

    maribao 3.0

--- scripts/maribao.js
+++ scripts/maribao.js
@@ -3,3 +3,12 @@ document.getElementById('currentyear').textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navigation = document.querySelector(".navigation");
+
+    hamburger.addEventListener("click", function () {
+        navigation.classList.toggle("active");
+    });
+});
