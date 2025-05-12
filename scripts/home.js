be6be6f5ca2b105133115a4a3bc79938983fafe7
Daniel25TE <danielalejandrosud25@gmail.com>
2025-05-12 07:45:48 -0600

    home 3.3

--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger-menu");
-    const nav = document.querySelector(".navigation");
+    const nav = document.querySelector("#animateme");
 
     hamburger.addEventListener("click", function () {
         nav.classList.toggle("active");
+        hamburger.classList.toggle("active");
     });
 
     document.addEventListener("click", function (event) {
         if (!hamburger.contains(event.target) && !nav.contains(event.target)) {
             nav.classList.remove("active");
+            hamburger.classList.remove("active");
         }
     });
 });
