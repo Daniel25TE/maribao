690ead51ce3491371c909c242241bb56d7db84fa
Daniel25TE <danielalejandrosud25@gmail.com>
2025-06-23 16:20:15 -0600

    maribaos backend 4.8

--- final/backend/public/js/admin.js
+++ final/backend/public/js/admin.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 tbody.appendChild(row);
             });
         } catch (err) {
-            console.error("Error al cargar reservas muchas:", err);
+            console.error("Error al cargar reservas mucha:", err);
             alert("No se pudieron cargar las reservas");
         }
     }
