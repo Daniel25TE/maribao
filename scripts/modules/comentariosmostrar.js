4cfa5424455646a48bff7f71d7b259305c419ed8
Daniel25TE <danielalejandrosud25@gmail.com>
2025-12-12 12:56:36 -0700

    comentarios placeholders test 1

--- scripts/modules/comentariosmostrar.js
+++ scripts/modules/comentariosmostrar.js
@@ -2,6 +2,15 @@ export async function cargarComentarios() {
     const contenedor = document.getElementById('comentarios');
     if (!contenedor) return;
 
+    
+    contenedor.innerHTML = `
+        <div class="comentario placeholder"><strong>González</strong> Buen servicio, todo tranquilo y seguro ⭐️⭐️⭐️⭐️⭐️</div>
+        <div class="comentario placeholder"><strong>Mina</strong> Tiene una buena ubicación respecto a la playa y a los restaurantes. El lugar está bastante bien. ⭐️⭐️⭐️⭐️⭐️</div>
+        <div class="comentario placeholder"><strong>Jacqueline</strong> Relájate al 100%
+            Su ubicación es excelente, mi familia y yo por lo general preferimos disfrutar de una playa tranquila y exclusiva. ⭐️⭐️⭐️⭐️⭐️</div>
+        <div class="comentario placeholder"><strong>Marco</strong> Muy tranquilo, cómodo en relación al precio, la atención de su dueño, cerca de la playa ⭐️⭐️⭐️⭐️⭐️</div>
+    `;
+
     try {
         const res = await fetch('https://hotel-backend-3jw7.onrender.com/api/comentarios');
         const comentarios = await res.json();
@@ -11,6 +20,7 @@ export async function cargarComentarios() {
             return;
         }
 
+        
         contenedor.innerHTML = comentarios
             .map(c => `<div class="comentario"><strong>${c.nombre}:</strong> ${c.comentario}</div>`)
             .join('');
@@ -19,5 +29,6 @@ export async function cargarComentarios() {
         console.error(err);
     }
 }
+
 
 
